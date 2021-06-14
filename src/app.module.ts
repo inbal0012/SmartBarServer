83fb2f34ae33563592d70a1448594e659e00384c
Inbal Schkolnik <capoeira0012>
2021-06-15 00:10:49 +0300

    Delete App Controller & Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose'
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { InventoryModule } from './inventory/inventory.module';
 import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [InventoryModule, MongooseModule.forRoot('mongodb://localhost:27017'), RecipeModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    InventoryModule,
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    RecipeModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
