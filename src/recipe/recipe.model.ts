fe9b561e3acefc90eea2deee225b30d9a1a9c1b9
Inbal Schkolnik <capoeira0012>
2021-06-20 11:22:49 +0300

    prettier

--- src/recipe/recipe.model.ts
+++ src/recipe/recipe.model.ts
@@ -1,42 +1,41 @@
+/* eslint-disable prettier/prettier */
 import * as mongoose from 'mongoose';
-import { IInventoryItem, InventoryItemSchema } from 'src/inventory/inventory.model';
+import {
+  IInventoryItem,
+  InventoryItemSchema,
+} from 'src/inventory/inventory.model';
 
-export const IngredientSchema = new mongoose.Schema(
-    {
-        productId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'InventoryItem',
-        },
-        amount: {
-            type: Number,
-            required: true,
-            min: [1, 'Quantity can not be less then 1.'],
-        },
-        optional: {
-            type: Boolean,
-            required: true,
-        },
-    }
-);
+export const IngredientSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'InventoryItem',
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: [1, 'Quantity can not be less then 1.'],
+  },
+  optional: {
+    type: Boolean,
+    required: true,
+  },
+});
 
 export const RecipeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    ingredients: { type: [IngredientSchema], required: true },
-    method: { type: [String], required: true },
-    portion: { type: Number, required: true },
+  name: { type: String, required: true },
+  ingredients: { type: [IngredientSchema], required: true },
+  method: { type: [String], required: true },
+  portion: { type: Number, required: true },
 });
-export interface IIngredient extends mongoose.Document {
-
-}
 
 export interface IRecipe extends mongoose.Document {
-    id: string;
-    name: string;
-    ingredients: {
-        amount: number,
-        productId: mongoose.Schema.Types.ObjectId,
-        optional: boolean
-    }[];
-    method: [string];
-    portion: number;
+  id: string;
+  name: string;
+  ingredients: {
+    amount: number;
+    productId: mongoose.Schema.Types.ObjectId;
+    optional: boolean;
+  }[];
+  method: [string];
+  portion: number;
 }
